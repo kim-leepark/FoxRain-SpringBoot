1468fcf2a3943309049cc3756736c2b599b2bb87
joungeun <lje0818ovo@naver.com>
2022-03-25 11:09:22 +0900

    [INIT]

---
+++ src/test/kotlin/com/dsm/fox/FoxApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dsm.fox
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FoxApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
